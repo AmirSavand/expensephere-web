f839ac867033b92b59041a9cbbcc990e48963b69
Amir Savand <amir@savandbros.com>
2020-05-11 12:14:08 +0300

    Add my wallets UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
-import { faCubes } from '@fortawesome/free-solid-svg-icons/faCubes';
-import { faMoneyBill } from '@fortawesome/free-solid-svg-icons/faMoneyBill';
+import { faLayerGroup } from '@fortawesome/free-solid-svg-icons/faLayerGroup';
 import { Color } from '@shared/classes/color';
 import { Category } from '@shared/interfaces/category';
+import { Profile } from '@shared/interfaces/profile';
+import { Wallet } from '@shared/interfaces/wallet';
 import { ApiService } from '@shared/services/api.service';
 
 @Component({
@@ -13,9 +14,14 @@ import { ApiService } from '@shared/services/api.service';
 })
 export class HomeComponent implements OnInit {
 
-  readonly faSample: IconDefinition = faMoneyBill;
+  readonly style = Color.style;
+  readonly colorsReserved = Color.COLORS_RESERVED;
 
-  style = Color.style;
+  readonly faWallets: IconDefinition = faLayerGroup;
+
+  profiles: Profile[];
+
+  wallets: Wallet[];
 
   categories: Category[];
 
@@ -23,6 +29,12 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.api.profile.list().subscribe((data: Profile[]): void => {
+      this.profiles = data;
+    });
+    this.api.wallet.list().subscribe((data: Wallet[]): void => {
+      this.wallets = data;
+    });
     this.api.category.list().subscribe((data: Category[]): void => {
       this.categories = data;
     });
