c6f5c6dc8353e8e44f79a5f3d6a460982b0dbc44
Amer Ansari <amer@savandbros.com>
2020-07-14 10:49:10 +0400

    (Add & Edit) profile modal

--- src/app/dash/profile/list/list.module.ts
+++ src/app/dash/profile/list/list.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ProfileFormModalModule } from '@shared/modules/profile-form-modal/profile-form-modal.module';
 
 import { ListRoutingModule } from './list-routing.module';
 import { ListComponent } from './list.component';
@@ -11,6 +13,8 @@ import { ListComponent } from './list.component';
   imports: [
     CommonModule,
     ListRoutingModule,
+    ProfileFormModalModule,
+    FontAwesomeModule,
   ],
 })
 export class ListModule {
