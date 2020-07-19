85961fd2a37bc81bb95e03a3eb7574af3a2c3fba
Amer Ansari <amer@savandbros.com>
2020-07-19 15:49:16 +0400

    Add category component

--- src/app/dash/dash-routing.module.ts
+++ src/app/dash/dash-routing.module.ts
@@ -25,6 +25,10 @@ const routes: Routes = [
         loadChildren: () => import('./event/event.module').then(m => m.EventModule),
       },
       {
+        path: 'category',
+        loadChildren: () => import('./category/category.module').then(m => m.CategoryModule),
+      },
+      {
         path: '**',
         pathMatch: 'full',
         redirectTo: 'overview',
