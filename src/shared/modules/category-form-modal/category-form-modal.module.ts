7b9e24800237e0d1ef9bdd0b0840c4fcca5e4529
Amer Ansari <amer@savandbros.com>
2020-07-20 18:34:27 +0400

    Change category type selection (#42)
    Co-authored-by: Amir Savand <amir@savandbros.com>

--- src/shared/modules/category-form-modal/category-form-modal.module.ts
+++ src/shared/modules/category-form-modal/category-form-modal.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SelectColorModule } from '@shared/modules/select-color/select-color.module';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { SelectModule } from '@shared/modules/select/select.module';
 import { CategoryFormModalComponent } from 'src/shared/modules/category-form-modal/category-form-modal.component';
 
@@ -13,6 +14,7 @@ import { CategoryFormModalComponent } from 'src/shared/modules/category-form-mod
     FontAwesomeModule,
     ReactiveFormsModule,
     SelectColorModule,
+    ButtonsModule,
     SelectModule,
   ],
   declarations: [
