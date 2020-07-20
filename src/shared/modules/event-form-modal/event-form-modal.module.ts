66f87dc6513208465aa2c24f6498cd198b60df6d
Amer Ansari <amer@savandbros.com>
2020-07-20 15:21:57 +0400

    Event new ui color

--- src/shared/modules/event-form-modal/event-form-modal.module.ts
+++ src/shared/modules/event-form-modal/event-form-modal.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SelectColorModule } from '@shared/modules/select-color/select-color.module';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { EventFormModalComponent } from './event-form-modal.component';
@@ -13,6 +14,7 @@ import { EventFormModalComponent } from './event-form-modal.component';
     FontAwesomeModule,
     ReactiveFormsModule,
     CollapseModule,
+    SelectColorModule,
   ],
   declarations: [
     EventFormModalComponent,
