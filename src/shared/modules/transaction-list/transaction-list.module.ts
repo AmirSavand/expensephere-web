53e0b1a8f8bfb96a76d383a759ed694e3cf99d22
Amer Ansari <amer@savandbros.com>
2020-07-27 07:07:24 +0400

    Change transaction editation UI

--- src/shared/modules/transaction-list/transaction-list.module.ts
+++ src/shared/modules/transaction-list/transaction-list.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ProfileCurrencyModule } from '@shared/modules/profile-currency/profile-currency.module';
 import { TransactionListComponent } from './transaction-list.component';
-
 
 
 @NgModule({
@@ -10,9 +11,12 @@ import { TransactionListComponent } from './transaction-list.component';
   imports: [
     CommonModule,
     ProfileCurrencyModule,
+    FontAwesomeModule,
+    RouterModule,
   ],
   exports: [
     TransactionListComponent,
   ],
 })
-export class TransactionListModule { }
+export class TransactionListModule {
+}
