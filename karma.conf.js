ca609192f932453e1069ff9e9e50902ee9090320
Amer Ansari <amer@savandbros.com>
2020-07-27 12:33:30 +0400

    Resolve "Rename to Expensephere" (#52)
    * Successfully renamed to expensephere 🤘️
    
    * Remove /api/ from Api url
    
    * Fix project name in text
    
    * Update angular.json
    
    Co-authored-by: Amir Savand <amir@savandbros.com>

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/openexpenses-web'),
+      dir: require('path').join(__dirname, './coverage/expensephere-web'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
