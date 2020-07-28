6dc48e336cfded45a85535e478b3b65ed13f6314
Amer Ansari <amer@savandbros.com>
2020-07-28 09:54:19 +0400

    Add filter

--- src/app/dash/wallet/list/list.module.ts
+++ src/app/dash/wallet/list/list.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { ListRoutingModule } from '@app/dash/wallet/list/list-routing.module';
 import { ListComponent } from '@app/dash/wallet/list/list.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FiltersModule } from '@shared/modules/filters/filters.module';
+import { LoadingModule } from '@shared/modules/loading/loading.module';
+import { NoDataModule } from '@shared/modules/no-data/no-data.module';
 import { ProfileCurrencyModule } from '@shared/modules/profile-currency/profile-currency.module';
 import { WalletFormModalModule } from '@shared/modules/wallet-form-modal/wallet-form-modal.module';
 import { WalletListModule } from '@shared/modules/wallet-list/wallet-list.module';
@@ -21,6 +24,9 @@ import { ModalModule } from 'ngx-bootstrap/modal';
     FontAwesomeModule,
     ProfileCurrencyModule,
     WalletListModule,
+    NoDataModule,
+    LoadingModule,
+    FiltersModule,
   ],
 })
 export class ListModule {
