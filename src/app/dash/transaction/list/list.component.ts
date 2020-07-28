caa0eb9ba5123b4fc8fbea95734968cf1a3b1e0f
Amer Ansari <amer@savandbros.com>
2020-07-28 14:05:29 +0400

    Add filter

--- src/app/dash/transaction/list/list.component.ts
+++ src/app/dash/transaction/list/list.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ExpenseKind } from '@shared/enums/kind';
 import { Category } from '@shared/interfaces/category';
+import { Event } from '@shared/interfaces/event';
+import { GetParams } from '@shared/interfaces/get-params';
 import { Transaction } from '@shared/interfaces/transaction';
 import { Wallet } from '@shared/interfaces/wallet';
 import { FilterType } from '@shared/modules/filters/shared/enums/filter-type';
 import { Filter } from '@shared/modules/filters/shared/interfaces/filter';
-import { FilterListValue } from '@shared/modules/filters/shared/interfaces/filter-list-value';
 import { ApiService } from '@shared/services/api.service';
 
 @Component({
@@ -15,32 +15,41 @@ import { ApiService } from '@shared/services/api.service';
 })
 export class ListComponent implements OnInit {
 
-  // readonly filters: Filter[] = [
-  //   {
-  //     type: FilterType.TEXT,
-  //     label: 'Search',
-  //     key: 'search',
-  //     value: '',
-  //   },
-  //   {
-  //     type: FilterType.LIST,
-  //     label: 'Wallet',
-  //     key: 'wallet',
-  //     value: '',
-  //     values: [
-  //       { label: 'Any', value: '' },
-  //       { label: 'Income', value: ExpenseKind.INCOME },
-  //       { label: 'Expense', value: ExpenseKind.EXPENSE },
-  //     ],
-  //   },
-  //   {
-  //     type: FilterType.LIST,
-  //     label: 'Wallet',
-  //     key: 'wallet',
-  //     value: '',
-  //     values: [],
-  //   },
-  // ];
+  readonly filters: Filter[] = [
+    {
+      type: FilterType.TEXT,
+      label: 'Search',
+      key: 'search',
+      value: '',
+    },
+    {
+      type: FilterType.LIST,
+      label: 'Wallet',
+      key: 'wallet',
+      value: '',
+      values: [
+        { label: 'Any', value: '' },
+      ],
+    },
+    {
+      type: FilterType.LIST,
+      label: 'Category',
+      key: 'category',
+      value: '',
+      values: [
+        { label: 'Any', value: '' },
+      ],
+    },
+    {
+      type: FilterType.LIST,
+      label: 'Event',
+      key: 'event',
+      value: '',
+      values: [
+        { label: 'Any', value: '' },
+      ],
+    },
+  ];
 
   /**
    * Wallet list
@@ -61,24 +70,57 @@ export class ListComponent implements OnInit {
   constructor(private api: ApiService) {
   }
 
+
+  /**
+   * Load transactions with filters
+   */
+  load(params: GetParams): void {
+    this.api.transaction.list(params).subscribe((data: Transaction[]): void => {
+      /**
+       * Filter out transfer types
+       */
+      this.transactions = data;
+    });
+  }
+
   ngOnInit(): void {
     /**
      * Load wallets
      */
     this.api.wallet.list().subscribe((data: Wallet[]): void => {
       this.wallets = data;
-    });
-    /**
-     * Load transactions
-     */
-    this.api.transaction.list().subscribe((data: Transaction[]): void => {
-      this.transactions = data;
+      // Set transaction list to filter
+      for (const wallet of data) {
+        this.filters[1].values.push({
+          label: wallet.name,
+          value: wallet.id,
+        });
+      }
     });
     /**
      * Load categories
      */
     this.api.category.list().subscribe((data: Category[]): void => {
       this.categories = data;
+      // Set category list to filter
+      for (const category of data) {
+        this.filters[2].values.push({
+          label: category.name,
+          value: category.id,
+        });
+      }
+    });
+    /**
+     * Load events
+     */
+    this.api.event.list().subscribe((data: Event[]): void => {
+      // Set event list to filter
+      for (const event of data) {
+        this.filters[3].values.push({
+          label: event.name,
+          value: event.id,
+        });
+      }
     });
   }
 }
