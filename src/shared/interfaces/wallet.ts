70b9cda3cb5e5715f8c337c3ccd0b3e9df40d9cc
Amir Savand <amir@savandbros.com>
2020-08-27 09:18:47 +0300

    Update wallet.ts

--- src/shared/interfaces/wallet.ts
+++ src/shared/interfaces/wallet.ts
@@ -3,10 +3,10 @@ import { Balance } from '@shared/interfaces/balance';
 export interface Wallet {
   readonly id: number;
   readonly profile: number;
+  readonly initial_balance?: number;
   name: string;
   color: string;
   icon: string;
   archive: boolean;
   balance: Balance;
-  initial_balance: number;
 }
