276b771c4767358a6e42ef86dc8060131a82afcd
Amer Ansari <amer@savandbros.com>
2020-08-27 10:21:39 +0400

    Resolve "Add initial balance input to wallet" (#89)
    * Add initial balance
    
    * Update overview.component.ts
    
    * Update overview.component.ts
    
    * Update wallet.ts
    
    * Update wallet-form-modal.component.html
    
    * Update wallet-form-modal.component.ts
    
    Co-authored-by: Amir Savand <amir@savandbros.com>

--- src/shared/interfaces/wallet.ts
+++ src/shared/interfaces/wallet.ts
@@ -3,6 +3,7 @@ import { Balance } from '@shared/interfaces/balance';
 export interface Wallet {
   readonly id: number;
   readonly profile: number;
+  readonly initial_balance?: number;
   name: string;
   color: string;
   icon: string;
