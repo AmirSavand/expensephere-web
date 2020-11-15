9485536b2eb1b7a55da1f2382397178e056ce0ac
Amir Savand <amir@savandbros.com>
2020-11-15 15:18:31 +0300

    Resolve "Upgrade to Angular 11 and other packages"
    Closes #139

--- src/shared/modules/profile-currency/profile-currency.pipe.ts
+++ src/shared/modules/profile-currency/profile-currency.pipe.ts
@@ -1,12 +1,24 @@
 import { CurrencyPipe } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
-import { ProfileService } from '@shared/services/profile.service';
+import { ProfileService } from 'src/shared/services/profile.service';
 
 @Pipe({
   name: 'profileCurrency',
 })
 export class ProfileCurrencyPipe extends CurrencyPipe implements PipeTransform {
-  transform(value: number, currency?: string): string {
-    return super.transform(value, currency || ProfileService.profile.value.currency, 'symbol-narrow');
+  transform(
+    value: number,
+    currencyCode?: string,
+    display?: 'code' | 'symbol' | 'symbol-narrow' | string | boolean,
+    digitsInfo?: string,
+    locale?: string,
+  ): string | any {
+    return super.transform(
+      value,
+      currencyCode || ProfileService.profile.value.currency,
+      'symbol-narrow',
+      digitsInfo,
+      locale,
+    );
   }
 }
