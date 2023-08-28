ec1677dcb73d83dc0b5dceabe9f242b806d97ac8
TatianaSavand <firefox100198@gmail.com>
2023-08-28 15:40:22 +0400

    Update with AM category-form-modal.

--- src/shared/modules/category-form-modal/category-form-modal.module.ts
+++ src/shared/modules/category-form-modal/category-form-modal.module.ts
@@ -6,6 +6,11 @@ import { SelectColorModule } from '@shared/modules/select-color/select-color.mod
 import { SelectModule } from '@shared/modules/select/select.module';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { CategoryFormModalComponent } from './category-form-modal.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
   imports: [
@@ -15,6 +20,11 @@ import { CategoryFormModalComponent } from './category-form-modal.component';
     SelectColorModule,
     ButtonsModule,
     SelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatButtonToggleModule,
   ],
   declarations: [
     CategoryFormModalComponent,
